15d38975f24533a8485b2d47cfeeaadcaed9425e
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-08-15 20:16:34 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,25 @@
+
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function (bills) {
+    const LEMONADE_COST = 5;
+    const COIN_VALUES = [5, 10, 20];
+    const frequencyAvailableValues = new Array(1 + COIN_VALUES[COIN_VALUES.length - 1]).fill(0);
+
+    for (let givenMoney of bills) {
+        ++frequencyAvailableValues[givenMoney];
+        let changeToReturn = givenMoney - LEMONADE_COST;
+
+        for (let i = COIN_VALUES.length - 1; i >= 0; --i) {
+            let numberOfCoins = Math.min(Math.floor(changeToReturn / COIN_VALUES[i]), frequencyAvailableValues[COIN_VALUES[i]]);
+            changeToReturn -= numberOfCoins * COIN_VALUES[i];
+            frequencyAvailableValues[COIN_VALUES[i]] -= numberOfCoins;
+        }
+        if (changeToReturn > 0) {
+            return false;
+        }
+    }
+    return true;
+};
